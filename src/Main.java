e90e1d6909f499ef4fdbc47dca3439fcbf497cc6
Emre Keles <station2506@gmail.com>
2023-01-08 13:20:52 +0300

    "last commit"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner km = new Scanner(System.in);
+
+        // Başlangıç 10 tl ile başlayıp, km başına 2.2 tl ödeme alan taksimetre ödeme alan programdır.
+        // istenilirse başlangıç ve km başına ücrette güncelleme yapılarak program
+        // üzerinden fiyat güncellemesi yapılabilir.
+
         double kilometre , tutar, toplamTutar ;
         double ilktutar = 10.0 ;
         System.out.print("Gidilen yolu km cinsinden giriniz = ");

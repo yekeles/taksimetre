ee68177810ba416b3c680f26c85d7f4ffba00f73
Emre Keles <station2506@gmail.com>
2023-01-08 12:39:10 +0300

    "son hal"

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner km = new Scanner(System.in);
         Double kilometre , tutar, toplamTutar ;
-        System.out.print("Kaç Kilometre Gidildi = ");
+        System.out.print("Gidilen yolu km cinsinden giriniz = ");
         kilometre = km.nextDouble();
-        toplamTutar = (kilometre * 2.2) ;
+        toplamTutar = (10 +(kilometre * 2.2)) ;
         tutar = toplamTutar < 20.0 ? 20 : toplamTutar ;
 
-        System.out.println(kilometre + " km gitiniz");
-        System.out.println(tutar + " ödeme yapmalısınız");
+        System.out.print(kilometre + " km gitiniz ve 20 Tl altında ücretlendirme sabittir. ");
+        System.out.print(tutar + " ödeme yapmalısınız");
 
     }
 }

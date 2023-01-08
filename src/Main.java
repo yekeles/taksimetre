931dd9a5b947c81d2f87ed927bcbab27fe56bdbf
Emre Keles <station2506@gmail.com>
2023-01-08 13:16:27 +0300

    "last commit"

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner km = new Scanner(System.in);
-        Double kilometre , tutar, toplamTutar ;
+        double kilometre , tutar, toplamTutar ;
+        double ilktutar = 10.0 ;
         System.out.print("Gidilen yolu km cinsinden giriniz = ");
         kilometre = km.nextDouble();
-        toplamTutar = (10 +(kilometre * 2.2)) ;
+        toplamTutar = (ilktutar +(kilometre * 2.2)) ;
         tutar = toplamTutar < 20.0 ? 20 : toplamTutar ;
 
         System.out.print(kilometre + " km gitiniz ve 20 Tl altında ücretlendirme sabittir. ");
